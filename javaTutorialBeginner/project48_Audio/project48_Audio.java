113f0826108f91ca26675c31a201601406f4409d
naveeharn <naveeharn@hotmail.com>
2021-03-10 13:37:36 +0700

    audio

---
+++ javaTutorialBeginner/project48_Audio/project48_Audio.java
@@ -0,0 +1,55 @@
+package javaTutorialBeginner.project48_Audio;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+// import javax.sound.sampled.AudioInputStream;
+// import javax.sound.sampled.AudioSystem;
+// import javax.sound.sampled.Clip;
+// import javax.sound.sampled.LineUnavailableException;
+// import javax.sound.sampled.UnsupportedAudioFileException;
+
+import javax.sound.sampled.*;
+
+
+public class project48_Audio {
+    
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        File file = new File("javaTutorialBeginner/project48_Audio/videoplayback (1).wav");
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+
+        clip.setMicrosecondPosition(20000000);
+        Scanner scanner = new Scanner(System.in);
+        String response = "";
+        
+ 
+        while (!response.equals("Q")) {
+            System.out.println("press P to play, press S to stop, press R to reset, press Q to Quit");
+            System.out.print("Enter your FUCKING choice : ");
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch (response) {
+                case "P":
+                    clip.start();
+                    break;
+                case "S":
+                    clip.stop();
+                    break; 
+                case "R":
+                    clip.setMicrosecondPosition(20000000);
+                    break;
+                default:
+
+                    break;
+            }
+        }
+
+        clip.start();
+
+        
+    }
+}
