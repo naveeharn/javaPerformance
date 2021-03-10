5d9b53d555c8a91ab6d120079f824c977fe0fc87
naveeharn <naveeharn@hotmail.com>
2021-03-10 10:36:43 +0700

    \n

--- javaTutorialBeginner/project45_fileClass/project45_fileClass.java
+++ javaTutorialBeginner/project45_fileClass/project45_fileClass.java
@@ -10,10 +10,10 @@ public class project45_fileClass {
          * an abstract representation of file and directory pathnames
          */
         
-        File file = new File("/Users/naveeharntehmarn/Desktop/Freshy/semester_2/java/javaTutorialBeginner/project45_fileClass/secretMessage.txt");
+        File file = new File("javaTutorialBeginner/project45_fileClass/secretMessage.txt");
 
         if (file.exists()) {
-            System.out.println("That file exists");
+            System.out.println("\nThat file exists");
             System.out.println(file.getPath());
             System.out.println(file.getAbsolutePath());
             System.out.println(file.isFile());
